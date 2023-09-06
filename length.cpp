b97cda7aae168c1510f2c47d687f17adf6f5c22e
John Frederick L. Milan <123443594+JFMilan06@users.noreply.github.com>
2023-09-06 20:33:24 +0800

    Add files via upload

---
+++ length.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int numbers[5]={5,4,3,2,1};
+	int len= sizeof(numbers)/sizeof(int);
+	cout<<"The size of the array is "<<len<<endl;
+	
+	
+	return 0;
+}
